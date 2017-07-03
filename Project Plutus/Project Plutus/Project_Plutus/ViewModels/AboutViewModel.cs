2d2d02c8dbb936d281149e44713d386a7bdaf988
seank842 <seank842@live.co.uk>
2017-07-03 21:15:16 +0100

    Revert "Deleted Project Plutus"
    This reverts commit 284657cf721e1835895cfd40e15792e13b5d5cb2.

---
+++ Project Plutus/Project Plutus/Project_Plutus/ViewModels/AboutViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Project_Plutus.ViewModels
+{
+	public class AboutViewModel : BaseViewModel
+	{
+		public AboutViewModel()
+		{
+			Title = "About";
+
+			OpenWebCommand = new Command(() => Device.OpenUri(new Uri("https://xamarin.com/platform")));
+		}
+
+		/// <summary>
+		/// Command to open browser to xamarin.com
+		/// </summary>
+		public ICommand OpenWebCommand { get; }
+	}
+}
---
+++ Project Plutus/Project Plutus/Project_Plutus/Views/AboutPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿
+using Xamarin.Forms;
+
+namespace Project_Plutus.Views
+{
+	public partial class AboutPage : ContentPage
+	{
+		public AboutPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
