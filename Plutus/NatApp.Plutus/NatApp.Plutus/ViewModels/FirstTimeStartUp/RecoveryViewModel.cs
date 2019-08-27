85908791589227258157f6106c7aefaf128b31bb
Sean Knox <seank842@live.co.uk>
2019-08-27 12:27:42 +0100

    Local DB recovery working, need to check exception dealing works at a later date

--- Plutus/NatApp.Plutus/NatApp.Plutus/ViewModels/FirstTimeStartUp/RecoveryViewModel.cs
+++ Plutus/NatApp.Plutus/NatApp.Plutus/ViewModels/FirstTimeStartUp/RecoveryViewModel.cs
@@ -1,4 +1,13 @@
-﻿using NatApp.Plutus.Helpers.Extensions;
+﻿using Database.Models;
+using NatApp.Plutus.Helpers.Extensions;
+using NatApp.Plutus.Services.IOHandeling;
+using NatApp.Plutus.Views;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using Xamarin.Essentials;
+using Xamarin.Forms;
 
 namespace NatApp.Plutus.ViewModels.FirstTimeStartUp
 {
@@ -9,5 +18,39 @@ namespace NatApp.Plutus.ViewModels.FirstTimeStartUp
             Title = "RecoveryTitle".Translate();
             Icon = "";
         }
+
+        #region Commands
+        Command _recoverDbCommand;
+
+        public Command RecoverDbCommand
+        {
+            get => _recoverDbCommand ?? (_recoverDbCommand = new Command(ExecuteRecoverDb));
+        }
+        #endregion
+
+        #region Execute Commands
+        private async void ExecuteRecoverDb()
+        {
+            var srcFile = await DependencyService.Get<IFile>().GetFile(new List<string> { ".db" });
+            await DependencyService.Get<IFile>().Copy(srcFile, FileSystem.AppDataDirectory, "Database.db");
+
+            try
+            {
+                using(var db = new Helpers.Database.Database(Database.Enums.DatabaseProvider.Sqlite))
+                {
+                    var item = db.Get<ItemModel>().FirstOrDefault();
+                }
+
+                DatabaseProviderSetting = Database.Enums.DatabaseProvider.Sqlite.ToString();
+                App.Current.MainPage = new LoginView();
+            }
+            catch(Exception ex)
+            {
+                await App.Current.MainPage.DisplayAlert("Hmm".Translate(), "CriticalIssue".Translate(), "OK".Translate());
+                Debug.WriteLine(ex.Message);
+                return;
+            }
+        }
+        #endregion
     }
 }
