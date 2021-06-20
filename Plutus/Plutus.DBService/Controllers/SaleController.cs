66901fd8297988158593a6dbac05405d581f71f6
Siddharth Agrawal <siddharthagrawal@outlook.com>
2021-06-21 00:08:55 +0530

    "Sales Report Restructutring"

--- Plutus/Plutus.DBService/Controllers/SaleController.cs
+++ Plutus/Plutus.DBService/Controllers/SaleController.cs
@@ -49,125 +49,21 @@ namespace Plutus.DBService.Controllers
                     .ThenInclude(r => r.CheckoutItemChange)
                 .Include(s => s.Employee).ToList();
 
-            var dailySalesSummaries = new List<IDictionary<string, Object>>();
-
-            var salesBreakdowns = new List<SalesBreakdown>();
-
             var currentDate = startDate;
-            do
-            {
-                var dailySalesSummary = new Dictionary<string, Object>();
-                dailySalesSummary.Add("Date", currentDate.ToShortDateString());
 
-                foreach (var payMethod in repositoryWrapper.PaymentMethodRepository.FindAll())
-                {
-                    dailySalesSummary.Add(
-                        payMethod.Name + $" ({"ExTax"})",
-                            data.Where(s => s.DateOfSale.Date.Equals(currentDate.Date) && s.Total != decimal.Zero && s.TotalExTax != decimal.Zero)
-                                .Sum(s => s.TotalExTax * (s.PaySales.Where(ps => ps.PayMethod.Id.Equals(payMethod.Id)).Sum(ps => ps.Amount - ps.Change) / s.Total)).Normalize());
-                }
+            // Fetch Sales Breakdowns
+            var salesBreakdowns = new SalesReport().fetchSalesBreakdown(currentDate, endDate, data);
 
-                dailySalesSummary.Add(
-                    "Daily (ex Tax)",
-                        data.Where(s => s.DateOfSale.Date.Equals(currentDate.Date)).Sum(s => s.TotalExTax));
-                dailySalesSummary.Add(
-                    "Daily (inc Tax)",
-                        data.Where(s => s.DateOfSale.Date.Equals(currentDate.Date)).Sum(s => s.Total));
+            // Fetch Sales Summaries
+            var dailySalesSummaries = new SalesReport().fetchdailySalesSummaries(currentDate, endDate, repositoryWrapper.PaymentMethodRepository.FindAll(), data);
 
-                dailySalesSummaries.Add(dailySalesSummary);
+            // Generate and get File Stream for Excel File containing Sales Breakdowns and Sales Summaries
+            var stream = new SalesReport().fetchSalesReportStream(dataSet, dailySalesSummaries, salesBreakdowns);
 
-                currentDate = currentDate.AddDays(1);
-            } while (currentDate <= endDate);
-
-            currentDate = startDate;
-            do
-            {
-                foreach (var salesData in data.Where(s => s.DateOfSale.Date.Equals(currentDate.Date)))
-                {
-                    foreach (var trans in salesData.Transactions)
-                    {
-                        //Record Transaction
-                        salesBreakdowns.Add(new SalesBreakdown()
-                        {
-                            RecordDate = currentDate.ToShortDateString(),
-                            SaleId = salesData.Id,
-                            ItemId = trans.Item.Id,
-                            ItemName = trans.Item.Name,
-                            UnitPriceAtCheckout = trans.CheckoutItemChangeId == null ? trans.ItemsCostPrice : trans.CheckoutItemChange.Price,
-                            UnitPriceAtCheckoutExTax = trans.CheckoutItemChangeId == null ? trans.ItemsCostExPrice : trans.CheckoutItemChange.ExPrice,
-                            Qty = trans.Amount,
-                            TotalSalePrice = (trans.CheckoutItemChangeId == null ? trans.ItemsCostPrice : trans.CheckoutItemChange.Price) * trans.Amount,
-                            TotalSalePriceExTax = (trans.CheckoutItemChangeId == null ? trans.ItemsCostExPrice : trans.CheckoutItemChange.ExPrice) * trans.Amount,
-                            EmployeeName = salesData.Employee.FullName
-                        });
-
-                        foreach (var transDiscount in trans.Transaction_Discounts)
-                        {
-                            var discountPrice = -decimal.Round(Math.Abs(transDiscount.Discount.Type == 0 ?
-                                transDiscount.DiscountRate :
-                                (trans.CheckoutItemChangeId == null ?
-                                    trans.ItemsCostPrice :
-                                    trans.CheckoutItemChange.Price)
-                                * transDiscount.DiscountRate), 2, MidpointRounding.AwayFromZero);
-                            var discountExPrice = -decimal.Round(Math.Abs(transDiscount.Discount.Type == 0 ?
-                                transDiscount.DiscountRate :
-                                (trans.CheckoutItemChangeId == null ?
-                                    trans.ItemsCostExPrice :
-                                    trans.CheckoutItemChange.ExPrice)
-                                * transDiscount.DiscountRate), 2, MidpointRounding.AwayFromZero);
-                            //Record Discount
-                            salesBreakdowns.Add(new SalesBreakdown()
-                            {
-                                RecordDate = currentDate.ToShortDateString(),
-                                SaleId = salesData.Id,
-                                ItemId = $"{"Discount"}",
-                                ItemName = $"{transDiscount.Discount.Name}, {trans.Item.Id}",
-                                UnitPriceAtCheckout = discountPrice,
-                                UnitPriceAtCheckoutExTax = discountExPrice,
-                                Qty = trans.Amount,
-                                TotalSalePrice = discountPrice * trans.Amount,
-                                TotalSalePriceExTax = discountExPrice * trans.Amount
-                            });
-                        }
-                    }
-
-                    foreach (var refund in salesData.Refunds)
-                        salesBreakdowns.Add(new SalesBreakdown()
-                        {
-                            RecordDate = currentDate.ToShortDateString(),
-                            SaleId = salesData.Id,
-                            ItemId = refund.Item.Id,
-                            ItemName = refund.Item.Name,
-                            UnitPriceAtCheckout = -Math.Abs(refund.CheckoutItemChangeId == null ? refund.Item.Price : refund.CheckoutItemChange.Price),
-                            UnitPriceAtCheckoutExTax = -Math.Abs(refund.CheckoutItemChangeId == null ? refund.Item.ExPrice : refund.CheckoutItemChange.ExPrice),
-                            Qty = refund.Amount,
-                            TotalSalePrice = -Math.Abs((refund.CheckoutItemChangeId == null ? refund.Item.Price : refund.CheckoutItemChange.Price)) * refund.Amount,
-                            TotalSalePriceExTax = -Math.Abs((refund.CheckoutItemChangeId == null ? refund.Item.ExPrice : refund.CheckoutItemChange.ExPrice)) * refund.Amount,
-                            EmployeeName = refund.AuthoriserId == null ? salesData.Employee.FullName : refund.Authoriser.FullName
-                        });
-                }
-
-                currentDate = currentDate.AddDays(1);
-            } while (currentDate <= endDate);
-
-            dataSet.Tables.Add(dailySalesSummaries.ToDataTable("DailySales"));
-            dataSet.Tables.Add(salesBreakdowns.ToDataTable("SalesBreakdown"));
-
-            return await new SalesReport().createExcel(dataSet, startDate, endDate); ;
-
-            /*using (var docHandler = new ExcelHandling())
-            {
-                docHandler.DataTableToWorksheet(dataSet);
-                var fileStream = docHandler.Finalize();
-                await DependencyService.Get<IFile>().SaveAndView(
-                        $"{"SalesReports".Translate()} - {startDate.ToShortDateString()}-{endDate.ToShortDateString()}",
-                        "application/vnd.ms-excel",
-                        fileStream,
-                        new Dictionary<string, IList<string>>() { { "Excel", new List<string>() { ".xlsx", ".xls" } } }
-                        );
-            }*/
-
-            //throw new NotImplementedException();
+            string fileName = $"{"SalesReports"} - {startDate.ToShortDateString()}-{endDate.ToShortDateString()}";
+            string fileType = "application/vnd.ms-excel";
+            
+            return File(stream.ToArray(), fileType, fileName);
         }
     }
 }
