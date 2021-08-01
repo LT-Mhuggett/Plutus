a509fcc82958a56218630064af5ec9742529f49f
Siddharth Agrawal <siddharthagrawal@outlook.com>
2021-08-01 22:42:21 +0530

    "Testing Setup for Repository"

---
+++ Plutus/Plutus.Repository.Tests/RepositoryTests.cs
@@ -0,0 +1,47 @@
+using NUnit.Framework;
+using Plutus.Entities.Models;
+
+namespace Plutus.Repository.Tests
+{
+    public class RepositoryTests : TestWithMySql
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        protected RepositoryWrapper RepositoryWrapper { get; set; }
+
+        public RepositoryTests() : base()
+        {
+            RepositoryWrapper = new RepositoryWrapper(DbContext);
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+
+        #region Data Persistance
+        [Test]
+        [Category("RecordPersistance")]
+        public void BussinessPersists()
+        {
+            var bussiness = new Bussiness()
+            {
+                Name = "Test Name",
+                NameAbbr = "TN"
+            };
+
+            RepositoryWrapper.BussinessRepository.Create(bussiness);
+            RepositoryWrapper.Save();
+
+            Assert.AreEqual(bussiness, DbContext.Bussiness.Find(bussiness.Id));
+            //Assert.AreEqual(1, DbContext.Bussiness.Count());
+        }
+
+        #endregion
+
+    }
+}
---
+++ Plutus/Plutus.Repository.Tests/TestWithMySql.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using Plutus.Entities;
+
+namespace Plutus.Repository.Tests
+{
+    public abstract class TestWithMySql : IDisposable
+    {
+        private const string InMemoryConnectionString = "DataSource=:memory:";
+        private readonly SqliteConnection _Connection;
+
+        protected readonly MySqlDbContext DbContext;
+
+        protected TestWithMySql()
+        {
+            _Connection = new SqliteConnection(InMemoryConnectionString);
+            _Connection.Open();
+            var options = new DbContextOptionsBuilder<RepositoryContext>()
+                .UseSqlite(_Connection)
+                .Options;
+            DbContext = new MySqlDbContext(options);
+            DbContext.Database.Migrate();
+        }
+
+        [TearDown]
+        public void DeleteDb()
+        {
+            foreach (var entry in DbContext.ChangeTracker.Entries())
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Modified:
+                    case EntityState.Deleted:
+                        entry.State = EntityState.Modified;
+                        entry.State = EntityState.Unchanged;
+                        break;
+
+                    case EntityState.Added:
+                        entry.State = EntityState.Detached;
+                        break;
+                }
+            }
+
+            DbContext.Bussiness.RemoveRange(DbContext.Bussiness);
+            /*DbContext.EmotionPlots.RemoveRange(DbContext.EmotionPlots);
+            DbContext.FacebookPages.RemoveRange(DbContext.FacebookPages);
+            DbContext.FacebookPosts.RemoveRange(DbContext.FacebookPosts);
+            DbContext.OnPremisesExtensionAttributes.RemoveRange(DbContext.OnPremisesExtensionAttributes);
+            DbContext.PostData.RemoveRange(DbContext.PostData);
+            DbContext.StaffPosts.RemoveRange(DbContext.StaffPosts);
+            DbContext.Tweets.RemoveRange(DbContext.Tweets);
+            DbContext.TwitterAccounts.RemoveRange(DbContext.TwitterAccounts);
+            DbContext.Users.RemoveRange(DbContext.Users);*/
+            DbContext.SaveChanges();
+        }
+
+        public void Dispose() => _Connection.Close();
+    }
+}
