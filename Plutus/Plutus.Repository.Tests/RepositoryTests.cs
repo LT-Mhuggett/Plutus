bdadfb473c7103795188cdd630d6bdeee7590c96
Siddharth Agrawal <siddharthagrawal@outlook.com>
2021-08-03 23:12:23 +0530

    "Store, Category and Discount Test Cases"

--- Plutus/Plutus.Repository.Tests/RepositoryTests.cs
+++ Plutus/Plutus.Repository.Tests/RepositoryTests.cs
@@ -39,12 +39,96 @@ namespace Plutus.Repository.Tests
             RepositoryWrapper.BussinessRepository.Create(bussiness);
             RepositoryWrapper.SetCurrentUser("Test");
             RepositoryWrapper.Save();
-            //RepositoryWrapper.EmotionDefinition.Create(emotionDefinition);
 
             Assert.AreEqual(bussiness, DbContext.Bussiness.Find(bussiness.Id));
             //Assert.AreEqual(1, DbContext.Bussiness.Count());
         }
 
+        [Test]
+        [Category("RecordPersistance")]
+        public void CategoryPersists()
+        {
+            var category = new Category()
+            {
+                Name = "Test Name",
+                Description = "Test Description"
+            };
+
+            RepositoryWrapper.CategoryRepository.Create(category);
+            RepositoryWrapper.SetCurrentUser("Test");
+            RepositoryWrapper.Save();
+
+            Assert.AreEqual(category, DbContext.Category.Find(category.Id));
+            //Assert.AreEqual(1, DbContext.Category.Count());
+        }
+
+        [Test]
+        [Category("RecordPersistance")]
+        public void StorePersists()
+        {
+            var bussiness = new Bussiness()
+            {
+                Name = "Test Name",
+                NameAbbr = "TN"
+            };
+
+            RepositoryWrapper.BussinessRepository.Create(bussiness);
+            RepositoryWrapper.SetCurrentUser("Test");
+            RepositoryWrapper.Save();
+
+            var store = new Store()
+            {
+                ContactNumber = "+449672513556",
+                AdLine1 = "Address Line 1",
+                AdLine2 = "Address Line 2",
+                City = "Test City",
+                PostCode = "201304",
+                Country = "England",
+                BussinessId = bussiness.Id
+            };
+
+            RepositoryWrapper.StoreRepository.Create(store);
+            RepositoryWrapper.SetCurrentUser("Test");
+            RepositoryWrapper.Save();
+
+            Assert.AreEqual(bussiness, DbContext.Bussiness.Find(bussiness.Id));
+            Assert.AreEqual(store, DbContext.Stores.Find(store.Id));
+        }
+
+        [Test]
+        [Category("RecordPersistance")]
+        public void DiscountPersists()
+        {
+            var bussiness = new Bussiness()
+            {
+                Name = "Test Name",
+                NameAbbr = "TN"
+            };
+
+            RepositoryWrapper.BussinessRepository.Create(bussiness);
+            RepositoryWrapper.SetCurrentUser("Test");
+            RepositoryWrapper.Save();
+
+            var discount = new Discount()
+            {
+                Name = "Test Name",
+                AllApplicable = true,
+                CanUseWithOtherDiscounts = false,
+                AutoApply = true,
+                Type = 1,
+                Amount = 13556,
+                UsesPerTransaction = 1,
+                RequiredNumOfItems = 1,
+                BussinessId = bussiness.Id
+            };
+
+            RepositoryWrapper.DiscountRepository.Create(discount);
+            RepositoryWrapper.SetCurrentUser("Test");
+            RepositoryWrapper.Save();
+
+            Assert.AreEqual(bussiness, DbContext.Bussiness.Find(bussiness.Id));
+            Assert.AreEqual(discount, DbContext.Discounts.Find(discount.Id));
+        }
 
         #endregion
 
@@ -89,7 +173,7 @@ namespace Plutus.Repository.Tests
             var currentTime = DateTime.UtcNow;
             var systemName = "Plutus.Repository.Tests";
 
-            var emotionDefinition = new Bussiness()
+            var bussiness = new Bussiness()
             {
                 Name = "Test Name",
                 NameAbbr = "TN",
@@ -102,17 +186,144 @@ namespace Plutus.Repository.Tests
             RepositoryWrapper.SetSyncState(true);
             await Task.Delay(10);
             RepositoryWrapper.SetCurrentUser(systemName);
-            await RepositoryWrapper.BussinessRepository.Create(emotionDefinition);
+            await RepositoryWrapper.BussinessRepository.Create(bussiness);
             RepositoryWrapper.Save();
             RepositoryWrapper.SetSyncState();
 
-            Assert.AreEqual(emotionDefinition.CreatedBy, systemName);
-            Assert.AreEqual(emotionDefinition.ModifiedBy, systemName);
-            Assert.AreEqual(emotionDefinition.CreatedAt, currentTime);
-            Assert.AreEqual(emotionDefinition.ModifiedAt, currentTime);
+            Assert.AreEqual(bussiness.CreatedBy, systemName);
+            Assert.AreEqual(bussiness.ModifiedBy, systemName);
+            Assert.AreEqual(bussiness.CreatedAt, currentTime);
+            Assert.AreEqual(bussiness.ModifiedAt, currentTime);
+        }
+
+        [Test]
+        [Category("SyncHandling")]
+        public async Task CategorySyncOnly()
+        {
+            var currentTime = DateTime.UtcNow;
+            var systemName = "Plutus.Repository.Tests";
+
+            var category = new Category()
+            {
+                Name = "Test Name",
+                Description = "Test Description",
+                CreatedAt = currentTime,
+                CreatedBy = systemName,
+                ModifiedAt = currentTime,
+                ModifiedBy = systemName
+            };
+
+            RepositoryWrapper.SetSyncState(true);
+            await Task.Delay(10);
+            RepositoryWrapper.SetCurrentUser(systemName);
+            await RepositoryWrapper.CategoryRepository.Create(category);
+            RepositoryWrapper.Save();
+            RepositoryWrapper.SetSyncState();
+
+            Assert.AreEqual(category.CreatedBy, systemName);
+            Assert.AreEqual(category.ModifiedBy, systemName);
+            Assert.AreEqual(category.CreatedAt, currentTime);
+            Assert.AreEqual(category.ModifiedAt, currentTime);
+        }
+
+        [Test]
+        [Category("SyncHandling")]
+        public async Task StoreSyncOnly()
+        {
+            var currentTime = DateTime.UtcNow;
+            var systemName = "Plutus.Repository.Tests";
+            var bussiness = new Bussiness()
+            {
+                Name = "Test Name",
+                NameAbbr = "TN",
+                CreatedAt = currentTime,
+                CreatedBy = systemName,
+                ModifiedAt = currentTime,
+                ModifiedBy = systemName
+            };
+
+            await RepositoryWrapper.BussinessRepository.Create(bussiness);
+            RepositoryWrapper.SetCurrentUser("Test");
+            RepositoryWrapper.Save();
+
+            var store = new Store()
+            {
+                ContactNumber = "+449672513556",
+                AdLine1 = "Address Line 1",
+                AdLine2 = "Address Line 2",
+                City = "Test City",
+                PostCode = "201304",
+                Country = "England",
+                BussinessId = bussiness.Id,
+                CreatedAt = currentTime,
+                CreatedBy = systemName,
+                ModifiedAt = currentTime,
+                ModifiedBy = systemName
+            };
+
+            RepositoryWrapper.SetSyncState(true);
+            await Task.Delay(10);
+            RepositoryWrapper.SetCurrentUser(systemName);
+            await RepositoryWrapper.StoreRepository.Create(store);
+            RepositoryWrapper.Save();
+            RepositoryWrapper.SetSyncState();
+
+            Assert.AreEqual(store.CreatedBy, systemName);
+            Assert.AreEqual(store.ModifiedBy, systemName);
+            Assert.AreEqual(store.CreatedAt, currentTime);
+            Assert.AreEqual(store.ModifiedAt, currentTime);
+        }
+
+        [Test]
+        [Category("SyncHandling")]
+        public async Task DiscountSyncOnly()
+        {
+            var currentTime = DateTime.UtcNow;
+            var systemName = "Plutus.Repository.Tests";
+            var bussiness = new Bussiness()
+            {
+                Name = "Test Name",
+                NameAbbr = "TN",
+                CreatedAt = currentTime,
+                CreatedBy = systemName,
+                ModifiedAt = currentTime,
+                ModifiedBy = systemName
+            };
+
+            await RepositoryWrapper.BussinessRepository.Create(bussiness);
+            RepositoryWrapper.SetCurrentUser("Test");
+            RepositoryWrapper.Save();
+
+            var discount = new Discount()
+            {
+                Name = "Test Name",
+                AllApplicable = true,
+                CanUseWithOtherDiscounts = false,
+                AutoApply = true,
+                Type = 1,
+                Amount = 13556,
+                UsesPerTransaction = 1,
+                RequiredNumOfItems = 1,
+                BussinessId = bussiness.Id,
+                CreatedAt = currentTime,
+                CreatedBy = systemName,
+                ModifiedAt = currentTime,
+                ModifiedBy = systemName
+            };
+
+            RepositoryWrapper.SetSyncState(true);
+            await Task.Delay(10);
+            RepositoryWrapper.SetCurrentUser(systemName);
+            await RepositoryWrapper.DiscountRepository.Create(discount);
+            RepositoryWrapper.Save();
+            RepositoryWrapper.SetSyncState();
+
+            Assert.AreEqual(discount.CreatedBy, systemName);
+            Assert.AreEqual(discount.ModifiedBy, systemName);
+            Assert.AreEqual(discount.CreatedAt, currentTime);
+            Assert.AreEqual(discount.ModifiedAt, currentTime);
         }
 
         #endregion
-
     }
 }
