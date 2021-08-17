1962c27d2b826a8cc35643634a5f8ec5df8edd1c
Siddharth Agrawal <siddharthagrawal@outlook.com>
2021-08-17 19:39:41 +0530

    "Load Test Authorisation flow added"

--- Plutus/Plutus.DBService/Tests/Stress/SpikeTesting.js
+++ Plutus/Plutus.DBService/Tests/Stress/SpikeTesting.js
@@ -1,6 +1,81 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
 
+const AZURE_TENANT_ID = 'ed398300-920d-4d36-9cde-5d3937f19b7b';
+const AZURE_CLIENT_ID = '131ab37b-4251-4c37-b0cc-ce3aaf390de2';
+const AZURE_CLIENT_SECRET = 'N9sSbvitt~aJN4XlNNCb5_HX.22_B77uq0';
+const USERNAME = 'isid@plutusdevenv.onmicrosoft.com';
+const PASSWORD = 'Anshul@123';
+const RESOURCE = 'https://plutusdevenv.onmicrosoft.com/131ab37b-4251-4c37-b0cc-ce3aaf390de2';
+const AZURE_SCOPES = 'https://plutusdevenv.onmicrosoft.com/131ab37b-4251-4c37-b0cc-ce3aaf390de2/Things.Read https://plutusdevenv.onmicrosoft.com/131ab37b-4251-4c37-b0cc-ce3aaf390de2/OtherThings.Read https://plutusdevenv.onmicrosoft.com/131ab37b-4251-4c37-b0cc-ce3aaf390de2/Permission.Write';
+
+export function setup() {
+    // Use either password authentication flow
+    let passwordAuthResp = authenticateUsingAzure(AZURE_TENANT_ID, AZURE_CLIENT_ID, AZURE_CLIENT_SECRET, AZURE_SCOPES, {
+        username: USERNAME,
+        password: PASSWORD,
+    });
+
+    return passwordAuthResp;
+
+    // Or client credentials authentication flow
+    // let clientAuthResp = authenticateUsingAzure(
+    //     AZURE_TENANT_ID, AZURE_CLIENT_ID, AZURE_CLIENT_SECRET, AZURE_SCOPES, RESOURCE
+    // );
+    // return clientAuthResp;
+
+    // // Example of Okta OAuth password authentication flow
+    // let oktaPassAuth = authenticateUsingOkta(OKTA_DOMAIN, 'default', OKTA_CLIENT_ID, OKTA_CLIENT_SECRET, OKTA_SCOPES,
+    // {
+    //     username: USERNAME,
+    //     password: PASSWORD
+    // });
+    // // This should print the authentication tokens
+    // console.log(JSON.stringify(oktaPassAuth));
+    // return oktaPassAuth;
+}
+
+
+
+/**
+ * Authenticate using OAuth against Azure Active Directory
+ * @function
+ * @param  {string} tenantId - Directory ID in Azure
+ * @param  {string} clientId - Application ID in Azure
+ * @param  {string} clientSecret - Can be obtained from https://docs.microsoft.com/en-us/azure/storage/common/storage-auth-aad-app#create-a-client-secret
+ * @param  {string} scope - Space-separated list of scopes (permissions) that are already given consent to by admin
+ * @param  {string} resource - Either a resource ID (as string) or an object containing username and password
+ */
+function authenticateUsingAzure(tenantId, clientId, clientSecret, scope, resource) {
+    let url;
+    const requestBody = {
+        client_id: clientId,
+        client_secret: clientSecret,
+        scope: scope,
+        admin_consent: true
+    };
+    if (typeof resource == 'string') {
+        //https://login.microsoftonline.com/ed398300-920d-4d36-9cde-5d3937f19b7b/oauth2/v2.0/authorize
+        url = `https://login.microsoftonline.com/${tenantId}/oauth2/v2.0/token`;
+        requestBody['grant_type'] = 'client_credentials';
+        requestBody['resource'] = resource;
+    } else if (
+        typeof resource == 'object' &&
+        resource.hasOwnProperty('username') &&
+        resource.hasOwnProperty('password')
+    ) {
+        url = `https://login.microsoftonline.com/${tenantId}/oauth2/v2.0/token`;
+        requestBody['grant_type'] = 'password';
+        requestBody['username'] = resource.username;
+        requestBody['password'] = resource.password;
+    } else {
+        throw 'resource should be either a string or an object containing username and password';
+    }
+
+    let response = http.post(url, requestBody);
+    return response.json();
+}
+
 export let options = {
     stages: [
         { duration: '10s', target: 100 }, // below normal load
@@ -12,13 +87,12 @@ export let options = {
         { duration: '10s', target: 0 },
     ],
 };
-export default function () {
+export default function (data) {
     const BASE_URL = 'http://localhost:58559/api'; // make sure this is not production
 
-    let apiToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6Im5PbzNaRHJPRFhFSzFqS1doWHNsSFJfS1hFZyJ9.eyJhdWQiOiIxMzFhYjM3Yi00MjUxLTRjMzctYjBjYy1jZTNhYWYzOTBkZTIiLCJpc3MiOiJodHRwczovL2xvZ2luLm1pY3Jvc29mdG9ubGluZS5jb20vZWQzOTgzMDAtOTIwZC00ZDM2LTljZGUtNWQzOTM3ZjE5YjdiL3YyLjAiLCJpYXQiOjE2MjkxMjEzNTcsIm5iZiI6MTYyOTEyMTM1NywiZXhwIjoxNjI5MTI1MjU3LCJhaW8iOiJBWVFBZS84VEFBQUFSRW8xcWxiRGhnOVY3TzJPS2xlK2hnbmpKZWJTQk9abXNKRWJtbERCTWd5dTc3c0NHNlUrM2U3cm01QTkxSXZPRUNKU2thbGQ1aVBMbmpRc05xS2lHdHMxYzNvUWpmbUJ4azhDdHJid09rWXZFMTVQQnVRY3h1ako1R0hyL095dEV6bkIyejB4WTVhRm4yNXhKSWQ3SVdnclJGcDFncmJMVFdLbnUzd1p3ZlE9IiwiYXpwIjoiMTMxYWIzN2ItNDI1MS00YzM3LWIwY2MtY2UzYWFmMzkwZGUyIiwiYXpwYWNyIjoiMCIsImlkcCI6ImxpdmUuY29tIiwibmFtZSI6IlNpZGRoYXJ0aCBBZ3Jhd2FsIiwib2lkIjoiNGYyZWJhZTMtMmRjYy00ODQxLWEwOWUtMzIwOTAzZjJhODUzIiwicHJlZmVycmVkX3VzZXJuYW1lIjoic2lkZGhhcnRoYWdyYXdhbEBvdXRsb29rLmNvbSIsInJoIjoiMC5BU0lBQUlNNTdRMlNOazJjM2wwNU5fR2JlM3V6R2hOUlFqZE1zTXpPT3E4NURlSWtBRk0uIiwic2NwIjoiVGhpbmdzLlJlYWQgT3RoZXJUaGluZ3MuUmVhZCBQZXJtaXNzaW9uLldyaXRlIiwic3ViIjoia3hLOGtmUTBYSGJVZm1RRVBQLVNXckJEZDh1TkY1UmkxNE4wdC1hSWc0YyIsInRpZCI6ImVkMzk4MzAwLTkyMGQtNGQzNi05Y2RlLTVkMzkzN2YxOWI3YiIsInV0aSI6Ikt4Y3RSalY4TjBhOXozZnBiaTB6QmciLCJ2ZXIiOiIyLjAifQ.XQ6gvFARe0IliSMCZwFv22abrPHpPFZEFvFKeHRdu85KA_1UhpU_wYbJDnb5FR0Y3d0baBtfRV3oTRWhx-hXDFSeTI9exhlUlQBGnqgjzVHBjL8PJww-f3HYkCxtlzdDTBOLK3WTJqOPsA3EptU7nQYKnPRlt_88fmWpXCC22aqT-f_7PUWhqGD0j9iaz0wKEksGE8woZsODoNn0t1U8oIfoBYIFN0DoIg7THQ3VAZD3FjohSWTsR_Y7JJKsyR3PbY_cMcjrNXECSCh9fOsjXRVmJc86Wzis9jmrZ8EijNIVY7gE1fDyNhHmDyv11M1-2PfSv1-L9--begM5ATZgDg';
     let params = {
         'User-Agent': 'k6',
-        Authorization: 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6Im5PbzNaRHJPRFhFSzFqS1doWHNsSFJfS1hFZyJ9.eyJhdWQiOiIxMzFhYjM3Yi00MjUxLTRjMzctYjBjYy1jZTNhYWYzOTBkZTIiLCJpc3MiOiJodHRwczovL2xvZ2luLm1pY3Jvc29mdG9ubGluZS5jb20vZWQzOTgzMDAtOTIwZC00ZDM2LTljZGUtNWQzOTM3ZjE5YjdiL3YyLjAiLCJpYXQiOjE2MjkxMjQ0MzYsIm5iZiI6MTYyOTEyNDQzNiwiZXhwIjoxNjI5MTI4MzM2LCJhaW8iOiJBWVFBZS84VEFBQUFjOGdFVTNFSGorWHRZd0lVVW13YjczMHl5bFpKVG03R0Ewb0R3RHZPdy9oYnAxQ0NCWHNuSnpxUVZiTk9ZUmdJYVNsZ1lIQlR3Tmhod1FYZ3FzcEtMRkRFQy93T0paSTF3Mjl6Tm9DaUlhMmVXSEU4OUJxMUtqR1ZzYzBHcXcxMXp4NXUxZUhKcEJzRjJuWHp5Sm5ieUhyUlFDSncvTjVHY1ZIUjNtdXUvZ009IiwiYXpwIjoiMTMxYWIzN2ItNDI1MS00YzM3LWIwY2MtY2UzYWFmMzkwZGUyIiwiYXpwYWNyIjoiMCIsImlkcCI6ImxpdmUuY29tIiwibmFtZSI6IlNpZGRoYXJ0aCBBZ3Jhd2FsIiwib2lkIjoiNGYyZWJhZTMtMmRjYy00ODQxLWEwOWUtMzIwOTAzZjJhODUzIiwicHJlZmVycmVkX3VzZXJuYW1lIjoic2lkZGhhcnRoYWdyYXdhbEBvdXRsb29rLmNvbSIsInJoIjoiMC5BU0lBQUlNNTdRMlNOazJjM2wwNU5fR2JlM3V6R2hOUlFqZE1zTXpPT3E4NURlSWtBRk0uIiwic2NwIjoiVGhpbmdzLlJlYWQgT3RoZXJUaGluZ3MuUmVhZCBQZXJtaXNzaW9uLldyaXRlIiwic3ViIjoia3hLOGtmUTBYSGJVZm1RRVBQLVNXckJEZDh1TkY1UmkxNE4wdC1hSWc0YyIsInRpZCI6ImVkMzk4MzAwLTkyMGQtNGQzNi05Y2RlLTVkMzkzN2YxOWI3YiIsInV0aSI6IjhiUU5hZ3hhdjBDbVZoeXhpcFh6QlEiLCJ2ZXIiOiIyLjAifQ.SLt_hIZXm4M-c9ik5xTTwpMJRRl3ZhMoxqsBo8qQn3yk-mpZg-dnMWqJ5jaPISWixKIJKpWg0IPVfPpiY1R_DMpApUgSETtdKZFVjI7eiDio3rY0h118V560GT3NSkmBuSjrnP7PA99qC-tCEz402eYrqCbCOiXthv3Ye-bA5xdbpQR0NcpGCKzX4ta5OPqdRfsdDETApXmFWUyR6lx2Nb07vVOd_X_G1rpwIwzf0RyCHs1Fkzsxo4QzdfB7-Or-7wG1R4mwyy9-qxf0-SiaGeLt-hfnBBkbHR8PaoCOE8T92qgFqR6slfAId9du5nL9PflhfKUkGQLTb9AJlBhF-w',
+        Authorization: `Bearer ${data.access_token}`, // or `Bearer ${clientAuthResp.access_token}`
     };
 
     let responses = http.batch([
--- Plutus/Plutus.DBService/Tests/Stress/sample.js
+++ Plutus/Plutus.DBService/Tests/Stress/sample.js
@@ -2,16 +2,15 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 
-/*const AZURE_TENANT_ID = 'AZURE_APP_DIRECTORY_ID';
+const AZURE_TENANT_ID = 'ed398300-920d-4d36-9cde-5d3937f19b7b';
 const AZURE_CLIENT_ID = '131ab37b-4251-4c37-b0cc-ce3aaf390de2';
-const AZURE_CLIENT_SECRET = 'AZURE_APP_CLIENT_SECRET';
-const USERNAME = 'USERNAME';
-const PASSWORD = 'PASSWORD';
-const RESOURCE = 'RESOURCE_ID_URI';
-const AZURE_SCOPES =
-    'email openid profile https://graph.microsoft.com/User.Read https://graph.microsoft.com/User.ReadBasic.All';*/
+const AZURE_CLIENT_SECRET = 'N9sSbvitt~aJN4XlNNCb5_HX.22_B77uq0';
+const USERNAME = 'isid@plutusdevenv.onmicrosoft.com';
+const PASSWORD = 'Anshul@123';
+const RESOURCE = 'https://plutusdevenv.onmicrosoft.com/131ab37b-4251-4c37-b0cc-ce3aaf390de2';
+const AZURE_SCOPES = 'https://plutusdevenv.onmicrosoft.com/131ab37b-4251-4c37-b0cc-ce3aaf390de2/Things.Read https://plutusdevenv.onmicrosoft.com/131ab37b-4251-4c37-b0cc-ce3aaf390de2/OtherThings.Read https://plutusdevenv.onmicrosoft.com/131ab37b-4251-4c37-b0cc-ce3aaf390de2/Permission.Write';
 
-/*function setup() {
+export function setup() {
     // Use either password authentication flow
     let passwordAuthResp = authenticateUsingAzure(AZURE_TENANT_ID, AZURE_CLIENT_ID, AZURE_CLIENT_SECRET, AZURE_SCOPES, {
         username: USERNAME,
@@ -35,7 +34,7 @@ const AZURE_SCOPES =
     // // This should print the authentication tokens
     // console.log(JSON.stringify(oktaPassAuth));
     // return oktaPassAuth;
-}*/
+}
    
 
     
@@ -47,17 +46,18 @@ const AZURE_SCOPES =
  * @param  {string} clientSecret - Can be obtained from https://docs.microsoft.com/en-us/azure/storage/common/storage-auth-aad-app#create-a-client-secret
  * @param  {string} scope - Space-separated list of scopes (permissions) that are already given consent to by admin
  * @param  {string} resource - Either a resource ID (as string) or an object containing username and password
- *//*
+ */
 function authenticateUsingAzure(tenantId, clientId, clientSecret, scope, resource) {
     let url;
     const requestBody = {
         client_id: clientId,
         client_secret: clientSecret,
         scope: scope,
+        admin_consent: true
     };
-
     if (typeof resource == 'string') {
-        url = `https://login.microsoftonline.com/${tenantId}/oauth2/token`;
+        //https://login.microsoftonline.com/ed398300-920d-4d36-9cde-5d3937f19b7b/oauth2/v2.0/authorize
+        url = `https://login.microsoftonline.com/${tenantId}/oauth2/v2.0/token`;
         requestBody['grant_type'] = 'client_credentials';
         requestBody['resource'] = resource;
     } else if (
@@ -74,9 +74,8 @@ function authenticateUsingAzure(tenantId, clientId, clientSecret, scope, resourc
     }
 
     let response = http.post(url, requestBody);
-
     return response.json();
-}*/
+}
 
 export let options = {
     insecureSkipTLSVerify: true,
@@ -85,45 +84,36 @@ export let options = {
     duration: '10s'
 };
 
-export default () => {
+export default (data) => {
 
-    let params = {
+   /* let params = {
         cookies: { my_cookie: 'value' },
         headers: { 'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6Im5PbzNaRHJPRFhFSzFqS1doWHNsSFJfS1hFZyJ9.eyJhdWQiOiIxMzFhYjM3Yi00MjUxLTRjMzctYjBjYy1jZTNhYWYzOTBkZTIiLCJpc3MiOiJodHRwczovL2xvZ2luLm1pY3Jvc29mdG9ubGluZS5jb20vZWQzOTgzMDAtOTIwZC00ZDM2LTljZGUtNWQzOTM3ZjE5YjdiL3YyLjAiLCJpYXQiOjE2MjkxMjEzNTcsIm5iZiI6MTYyOTEyMTM1NywiZXhwIjoxNjI5MTI1MjU3LCJhaW8iOiJBWVFBZS84VEFBQUFSRW8xcWxiRGhnOVY3TzJPS2xlK2hnbmpKZWJTQk9abXNKRWJtbERCTWd5dTc3c0NHNlUrM2U3cm01QTkxSXZPRUNKU2thbGQ1aVBMbmpRc05xS2lHdHMxYzNvUWpmbUJ4azhDdHJid09rWXZFMTVQQnVRY3h1ako1R0hyL095dEV6bkIyejB4WTVhRm4yNXhKSWQ3SVdnclJGcDFncmJMVFdLbnUzd1p3ZlE9IiwiYXpwIjoiMTMxYWIzN2ItNDI1MS00YzM3LWIwY2MtY2UzYWFmMzkwZGUyIiwiYXpwYWNyIjoiMCIsImlkcCI6ImxpdmUuY29tIiwibmFtZSI6IlNpZGRoYXJ0aCBBZ3Jhd2FsIiwib2lkIjoiNGYyZWJhZTMtMmRjYy00ODQxLWEwOWUtMzIwOTAzZjJhODUzIiwicHJlZmVycmVkX3VzZXJuYW1lIjoic2lkZGhhcnRoYWdyYXdhbEBvdXRsb29rLmNvbSIsInJoIjoiMC5BU0lBQUlNNTdRMlNOazJjM2wwNU5fR2JlM3V6R2hOUlFqZE1zTXpPT3E4NURlSWtBRk0uIiwic2NwIjoiVGhpbmdzLlJlYWQgT3RoZXJUaGluZ3MuUmVhZCBQZXJtaXNzaW9uLldyaXRlIiwic3ViIjoia3hLOGtmUTBYSGJVZm1RRVBQLVNXckJEZDh1TkY1UmkxNE4wdC1hSWc0YyIsInRpZCI6ImVkMzk4MzAwLTkyMGQtNGQzNi05Y2RlLTVkMzkzN2YxOWI3YiIsInV0aSI6Ikt4Y3RSalY4TjBhOXozZnBiaTB6QmciLCJ2ZXIiOiIyLjAifQ.XQ6gvFARe0IliSMCZwFv22abrPHpPFZEFvFKeHRdu85KA_1UhpU_wYbJDnb5FR0Y3d0baBtfRV3oTRWhx-hXDFSeTI9exhlUlQBGnqgjzVHBjL8PJww-f3HYkCxtlzdDTBOLK3WTJqOPsA3EptU7nQYKnPRlt_88fmWpXCC22aqT-f_7PUWhqGD0j9iaz0wKEksGE8woZsODoNn0t1U8oIfoBYIFN0DoIg7THQ3VAZD3FjohSWTsR_Y7JJKsyR3PbY_cMcjrNXECSCh9fOsjXRVmJc86Wzis9jmrZ8EijNIVY7gE1fDyNhHmDyv11M1-2PfSv1-L9--begM5ATZgDg' },
         redirects: 5,
         tags: { k6test: 'yes' },
+    };*/
+    let params = {
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${data.access_token}`, // or `Bearer ${clientAuthResp.access_token}`
+        },
     };
 
     http.get('http://localhost:58559/api/AuthAction/Index', params);
-    sleep(1);
     http.get('http://localhost:58559/api/Bussiness/Index', params);
-    sleep(1);
     http.get('http://localhost:58559/api/Category/Index', params);
-    sleep(1);
     http.get('http://localhost:58559/api/Discount/Index', params);
-    sleep(1);
     http.get('http://localhost:58559/api/Employee/Index', params);
-    sleep(1);
     http.get('http://localhost:58559/api/Item/Index', params);
-    sleep(1);
     http.get('http://localhost:58559/api/Note/Index', params);
-    sleep(1);
     http.get('http://localhost:58559/api/PaymentMethod/Index', params);
-    sleep(1);
     http.get('http://localhost:58559/api/Refund/Index', params);
-    sleep(1);
     http.get('http://localhost:58559/api/Sale/Index', params);
-    sleep(1);
     http.get('http://localhost:58559/api/SavedTransaction/Index', params);
-    sleep(1);
     http.get('http://localhost:58559/api/Store/Index', params);
-    sleep(1);
     http.get('http://localhost:58559/api/Stock/Index', params);
-    sleep(1);
     http.get('http://localhost:58559/api/Tax/Index', params);
-    sleep(1);
     http.get('http://localhost:58559/api/Till/Index', params);
-    sleep(1);
     http.get('http://localhost:58559/api/Transaction/Index', params);
     sleep(1);
 };
